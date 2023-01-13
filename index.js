664e4156580c3211ca9079d4f0ae84782eb3a280
sandeshjadhav5 <jadhavs932@gmail.com>
2023-01-13 22:08:26 +0530

    added backend files

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { connection } = require("./configs/db");
+const { userRouter } = require("./routes/User.routes");
+const { notesRouter } = require("./routes/Notes.routes");
+const { authenticate } = require("./middlewares/authenticate.middleware");
+require("dotenv").config();
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+app.use("/users", userRouter);
+app.use(authenticate);
+app.use("/notes", notesRouter);
+
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log("connected to the DB");
+  } catch (err) {
+    console.log(err);
+  }
+  console.log(`listening at port : ${process.env.port}`);
+});
+
+/*
+{
+  "title": "full stack",
+  "note": "learning full stack crud",
+  "category": "web dev",
+  "author": "sandy"
+}
+*/
---
+++ routes/Notes.routes.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const { NoteModel } = require("../models/Notes.model");
+
+const notesRouter = express.Router();
+
+// G E T
+notesRouter.get("/", (req, res) => {
+  res.send("All Notes ");
+});
+
+// P O S T
+notesRouter.post("/create", async (req, res) => {
+  const payload = req.body;
+
+  try {
+    const new_note = new NoteModel(payload);
+    await new_note.save();
+    res.send({ msg: "Note Created" });
+  } catch (err) {
+    console.log(err);
+    res.send({ msg: "Something Went Wrong" });
+  }
+});
+
+// P A T C H
+notesRouter.patch("/update/:id", async (req, res) => {
+  const payload = req.body;
+  const id = req.params.id;
+  const note = await NoteModel.find({ _id: id });
+  const userID_in_note = note.userID;
+  const userID_making_req = req.body.userID;
+
+  try {
+    if (userID_making_req !== userID_in_note) {
+      res.send({ msg: "You Are Not Authorised" });
+    } else {
+      await NoteModel.findByIdAndUpdate({ _id: id }, payload);
+      res.send("updated the Note");
+    }
+  } catch (err) {
+    res.send({ msg: "Something Went Wrong" });
+  }
+});
+
+//D E L E T E
+notesRouter.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    await NoteModel.findByIdAndDelete({ _id: id });
+    res.send("Deleted the Note");
+  } catch (err) {
+    console.log(err);
+    res.send({ msg: "Something Went Wrong" });
+  }
+});
+
+module.exports = {
+  notesRouter,
+};
